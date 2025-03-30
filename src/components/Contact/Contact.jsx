c9ec8831194008b3ba4a572ff270542415281999
ankhod <ankhod7@gmail.com>
2025-03-30 20:46:25 +0300

    test-1

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,6 @@
 import { useDispatch } from 'react-redux';
 import { deleteContact } from '../../redux/contactsSlice';
+import { FaUser, FaPhone } from 'react-icons/fa';
 import css from './Contact.module.css';
 
 export default function Contact({ contact }) {
@@ -11,10 +12,19 @@ export default function Contact({ contact }) {
 
   return (
     <li className={css.contact}>
-      <span>
-        {contact.name}: {contact.number}
-      </span>
-      <button onClick={handleDelete}>Delete</button>
+      <div className={css.contactInfo}>
+        <div className={css.infoRow}>
+          <FaUser />
+          <span className={css.name}>{contact.name}</span>
+        </div>
+        <div className={css.infoRow}>
+          <FaPhone />
+          <span className={css.number}>{contact.number}</span>
+        </div>
+      </div>
+      <button onClick={handleDelete} className={css.deleteBtn}>
+        Delete
+      </button>
     </li>
   );
 }
